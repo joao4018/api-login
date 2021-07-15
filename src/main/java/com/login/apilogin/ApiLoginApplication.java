6c3ac829d0d064867c0b7272d1dd4bcaf534b8bf
joao.reis <joao.reis@valecard.com.br>
2021-07-14 21:34:40 -0300

    Iniciando projeto

---
+++ src/main/java/com/login/apilogin/ApiLoginApplication.java
@@ -0,0 +1,13 @@
+package com.login.apilogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/login/apilogin/ApiLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.apilogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
