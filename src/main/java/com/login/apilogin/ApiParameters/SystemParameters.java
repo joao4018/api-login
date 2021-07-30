4b4daf3a2afbf3c96e47e55dbf47f8e4bd314833
joao4018 <joao4018@gmail.com>
2021-07-29 23:00:01 -0300

    Creating static fields for the system parameters

---
+++ src/main/java/com/login/apilogin/ApiParameters/SystemParameters.java
@@ -0,0 +1,14 @@
+package com.login.apilogin.ApiParameters;
+
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+import static org.hibernate.type.IntegerType.ZERO;
+
+@NoArgsConstructor
+public class SystemParameters {
+    public static final Integer PAGE_SIZE = BigDecimal.valueOf(5L).intValue();
+    public static final Integer PAGE_INIT = ZERO;
+
+}
