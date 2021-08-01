a2bbd6e163499442c73aede2ba308acc4f1e936d
joao4018 <joao4018@gmail.com>
2021-08-01 17:19:54 -0300

    Adding constants for tests

---
+++ src/test/java/com/login/apilogin/constants/ConstantsForTest.java
@@ -0,0 +1,8 @@
+package com.login.apilogin.constants;
+
+public class ConstantsForTest {
+    public static final String DEFAULT_STREET_TEST = "Park Avenue (Fourth Avenue)";
+
+    public static final String DEFAULT_STREET_TEST2 = "Ocean City Beach";
+
+}
