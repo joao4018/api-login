b06a37dec61435cb4a7364be83a1d31b939ea376
joao4018 <joao4018@gmail.com>
2021-08-11 00:09:44 -0300

    Create: jwt token

---
+++ src/main/java/com/login/apilogin/config/jwt/JwtConfiguration.java
@@ -0,0 +1,32 @@
+package com.login.apilogin.config.jwt;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author joao4018
+ */
+@Configuration
+@ConfigurationProperties(prefix = "security")
+@Getter
+@Setter
+@ToString
+public class JwtConfiguration {
+    private String loginUrl = "/login/**";
+    @NestedConfigurationProperty
+    private Header header = new Header();
+    private int expiration = 3600;
+    private String privateKey = "qxBEEQv7E8aviX1KUcdOiF5ve5COUPAr";
+    private String type = "encrypted";
+
+    @Getter
+    @Setter
+    public static class Header {
+        private String name = "Authorization";
+        private String prefix = "Bearer ";
+    }
+}
---
+++ src/main/java/com/login/apilogin/config/jwt/filter/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -0,0 +1,68 @@
+package com.login.apilogin.config.jwt.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.login.apilogin.config.jwt.JwtConfiguration;
+import com.login.apilogin.domain.impl.AccessUser;
+import com.login.apilogin.token.token.creator.TokenCreator;
+import com.nimbusds.jwt.SignedJWT;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static java.util.Collections.emptyList;
+
+/**
+ * @author Joao4018
+ */
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Slf4j
+public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+    private final JwtConfiguration jwtConfiguration;
+    private final TokenCreator tokenCreator;
+
+    @Override
+    @SneakyThrows
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
+        log.info("Attempting authentication. . .");
+        AccessUser applicationUser = new ObjectMapper().readValue(request.getInputStream(), AccessUser.class);
+
+        if (applicationUser == null)
+            throw new UsernameNotFoundException("Unable to retrieve the username or password");
+
+        log.info("Creating the authentication object for the user '{}' and calling UserDetailServiceImpl loadUserByUsername", applicationUser.getUsername());
+
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(applicationUser.getUsername(), applicationUser.getPassword(), emptyList());
+
+        usernamePasswordAuthenticationToken.setDetails(applicationUser);
+
+        return authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+    }
+
+    @Override
+    @SneakyThrows
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication auth) {
+        log.info("Authentication was successful for the user '{}', generating JWE token", auth.getName());
+
+        SignedJWT signedJWT = tokenCreator.createSignedJWT(auth);
+
+        String encryptedToken = tokenCreator.encryptToken(signedJWT);
+
+        log.info("Token generated successfully, adding it to the response header");
+
+        response.addHeader("Access-Control-Expose-Headers", "XSRF-TOKEN, " + jwtConfiguration.getHeader().getName());
+
+        response.addHeader(jwtConfiguration.getHeader().getName(), jwtConfiguration.getHeader().getPrefix() + encryptedToken);
+    }
+
+}
