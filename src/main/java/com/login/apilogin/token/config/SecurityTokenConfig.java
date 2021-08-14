5c758957a3e1b29f04222e6ffd2a4c2459182264
joao4018 <joao4018@gmail.com>
2021-08-14 14:12:48 -0300

    Configure: doc auth with token

--- src/main/java/com/login/apilogin/token/config/SecurityTokenConfig.java
+++ src/main/java/com/login/apilogin/token/config/SecurityTokenConfig.java
@@ -35,6 +35,7 @@ public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(jwtConfiguration.getLoginUrl()).permitAll()
                 .antMatchers("/swagger-ui/**/*").permitAll()
                 .antMatchers("/v3/api-docs/**/*").permitAll()
+                .antMatchers("/v3/api-docs").permitAll()
                 .anyRequest()
                 .authenticated();
     }
