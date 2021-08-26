15e753584a5a56077b48274f942ade68439b81bb
joao4018 <joao4018@gmail.com>
2021-08-26 13:13:19 -0300

    Configure: configure eureka + gateway

--- src/main/java/com/login/apilogin/ApiLoginApplication.java
+++ src/main/java/com/login/apilogin/ApiLoginApplication.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import static com.login.apilogin.constants.Asg.Asg.AUTHOR_LINKEDIN;
 import static com.login.apilogin.constants.Asg.Asg.AUTHOR_SIGNATURE;
@@ -13,6 +14,7 @@ import static com.login.apilogin.constants.Asg.Asg.AUTHOR_SIGNATURE;
 @SpringBootApplication
 @Log4j2
 @EnableConfigurationProperties(value = JwtConfiguration.class)
+@EnableEurekaClient
 public class ApiLoginApplication {
 
     public static void main(String[] args) {
