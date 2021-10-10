5bfdd42b18850c7c10f6cca477829630c5a2e48e
joao4018 <joao4018@gmail.com>
2021-10-10 14:22:35 -0300

    Configure: grafana

--- src/main/java/com/login/apilogin/ApiLoginApplication.java
+++ src/main/java/com/login/apilogin/ApiLoginApplication.java
@@ -1,11 +1,15 @@
 package com.login.apilogin;
 
 import com.login.apilogin.config.jwt.JwtConfiguration;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 import static com.login.apilogin.constants.Asg.Asg.AUTHOR_LINKEDIN;
 import static com.login.apilogin.constants.Asg.Asg.AUTHOR_SIGNATURE;
@@ -21,6 +25,12 @@ public class ApiLoginApplication {
         log.info(AUTHOR_SIGNATURE);
         log.info(AUTHOR_LINKEDIN);
         SpringApplication.run(ApiLoginApplication.class, args);
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(
+            @Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
     }
 
 }
