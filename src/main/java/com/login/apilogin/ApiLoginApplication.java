e7a201afb77507ae4d7722d28d6f051049f75ad3
joao4018 <joao4018@gmail.com>
2022-07-25 22:55:59 -0300

    Fix managements discovery

--- src/main/java/com/login/apilogin/ApiLoginApplication.java
+++ src/main/java/com/login/apilogin/ApiLoginApplication.java
@@ -5,7 +5,7 @@ import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+//import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -28,10 +28,10 @@ public class ApiLoginApplication {
         SpringApplication.run(ApiLoginApplication.class, args);
     }
 
-    @Bean
-    MeterRegistryCustomizer<MeterRegistry> configurer(
-            @Value("${spring.application.name}") String applicationName) {
-        return (registry) -> registry.config().commonTags("application", applicationName);
-    }
+//    @Bean
+//    MeterRegistryCustomizer<MeterRegistry> configurer(
+//            @Value("${spring.application.name}") String applicationName) {
+//        return (registry) -> registry.config().commonTags("application", applicationName);
+//    }
 
 }
