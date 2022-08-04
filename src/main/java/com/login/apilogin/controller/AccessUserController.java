4faa4a69ce5226eaaec8498223ab0dca1e911275
joao4018 <joao4018@gmail.com>
2022-08-03 21:56:56 -0300

    fixed response bug
    Co-authored-by: MatheusFilipe95 <mafieu666@hotmail.com>

--- src/main/java/com/login/apilogin/controller/AccessUserController.java
+++ src/main/java/com/login/apilogin/controller/AccessUserController.java
@@ -1,13 +1,10 @@
 package com.login.apilogin.controller;
 
-import com.login.apilogin.domain.impl.AccessUser;
 import com.login.apilogin.request.AccessLoginPostRequestBody;
 import com.login.apilogin.request.AccessPostRequestBody;
 import com.login.apilogin.request.AccessRecoveryPostRequestBody;
 import com.login.apilogin.request.PersonalDataPostRequestBody;
-import com.login.apilogin.response.GenericResponse;
 import com.login.apilogin.response.ResponseBody;
-import com.login.apilogin.response.SignupPostResponseBody;
 import com.login.apilogin.service.impl.AccessUserServiceImpl;
 import com.login.apilogin.util.DateUtil;
 import io.swagger.v3.oas.annotations.Operation;
@@ -32,7 +29,7 @@ import static com.login.apilogin.constants.SystemConstantsExceptions.IMPLEMENTED
 @RestController
 @Log4j2
 @RequiredArgsConstructor
-public class AccessUserController extends AbstractController{
+public class AccessUserController extends AbstractController {
 
     private final DateUtil dateUtil;
     private final AccessUserServiceImpl accessUserService;
@@ -46,54 +43,32 @@ public class AccessUserController extends AbstractController{
     @PostMapping(path = "/signup")
     @Operation(summary = "Create a new user")
     public ResponseEntity<ResponseBody> save(@RequestBody @Valid AccessPostRequestBody accessRequestBody) {
-        return new ResponseEntity<>(buildResponsyBody(accessUserService
-                .createUser(accessRequestBody),
-                "Operação de cadastro",
-                "Create a new user"
-                ), HttpStatus.CREATED);
+        return new ResponseEntity<>(buildResponsyBody(accessUserService.createUser(accessRequestBody), "Operação de cadastro", "Create a new user"), HttpStatus.CREATED);
     }
 
     @PostMapping(path = "/recoveryPassword")
-
     @Operation(summary = "Recovery a user")
-    public ResponseEntity<ResponseBody> recovery(
-            @RequestBody @Valid AccessRecoveryPostRequestBody accessRequestBody) {
-        return new ResponseEntity<>(buildResponsyBody(
-                accessUserService.recoveryPassword(accessRequestBody),
-                "Operação de recuperação de usuário",
-                "Recovery a user"
-        ), HttpStatus.CREATED);
+    public ResponseEntity<ResponseBody> recovery(@RequestBody @Valid AccessRecoveryPostRequestBody accessRequestBody) {
+        return new ResponseEntity<>(buildResponsyBody(accessUserService.recoveryPassword(accessRequestBody), "Operação de recuperação de usuário", "Recovery a user"), HttpStatus.CREATED);
     }
 
     @PostMapping(path = "/registerPersonalData")
     @Operation(summary = "register data an user")
-    public ResponseEntity<ResponseBody> registerPersonalData(
-            @RequestBody @Valid PersonalDataPostRequestBody personalDataPostRequestBody,
-            @RequestParam String username ) {
-        return new ResponseEntity<>(buildResponsyBody(
-                accessUserService.addPersonalDataAtUser(personalDataPostRequestBody, username),
-                "Operação de cadastro de dados pessoais",
-                "register data an user"
-        ), HttpStatus.CREATED);
+    public ResponseEntity<ResponseBody> registerPersonalData(@RequestBody @Valid PersonalDataPostRequestBody personalDataPostRequestBody, @RequestParam String username) {
+        return new ResponseEntity<>(buildResponsyBody(accessUserService.addPersonalDataAtUser(personalDataPostRequestBody, username), "Operação de cadastro de dados pessoais", "register data an user"), HttpStatus.CREATED);
     }
 
     @GetMapping(path = "/user")
     @Operation(summary = "Returns an existing user")
-    public ResponseEntity<ResponseBody> getUser(
-        @RequestParam @Valid @NotEmpty(message = "The email cannot be empty") String email ) {
-       return new ResponseEntity<>(buildResponsyBody(
-               accessUserService.searchUserByEmail(email),
-               "Operação de busca de usuário",
-               "Returns an existing user"
-       ), HttpStatus.OK);
+    public ResponseEntity<ResponseBody> getUser(@RequestParam @Valid @NotEmpty(message = "The email cannot be empty") String email) {
+        return new ResponseEntity<>(buildResponsyBody(accessUserService.searchUserByEmail(email), "Operação de busca de usuário", "Returns an existing user"), HttpStatus.OK);
     }
 
     @GetMapping(path = "/validateUserAccount")
     @Operation(summary = "validate an existing user")
-    public ResponseEntity<HttpStatus> validateUserAccount(
-        @RequestParam String email ) {
+    public ResponseEntity<HttpStatus> validateUserAccount(@RequestParam String email) {
         accessUserService.validateUserAccount(email);
-       return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 
 }
